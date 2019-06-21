81da19e9b32c5273ee5279aaf62e832575a7e26c
octopoes <45490891+Lieuwe41@users.noreply.github.com>
2019-06-21 15:56:01 +0200

    input vragen

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 uitleg = "..."
 print(uitleg)
 
+#random getalen reeks pakken
 import random
 list = ["1", "2"]
 print (random.choice(list))
+
+#niput van user vragen
+userInput = input("geef")
+print(userInput)
+#check input
