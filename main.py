833bf6f43817868c32138c0d88e1c53643912767
octopoes <45490891+Lieuwe41@users.noreply.github.com>
2019-06-21 16:21:58 +0200

    Check input

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ print (random.choice(list))
 userInput = input("geef")
 print(userInput)
 #check input
+if userInput == randomcode:
+  print ("Good job! You cracked the code!")
+else :
+  print ("Try again!")
