0e34d20d64aa51539741fd70fb1bc554a80a0150
octopoes <45490891+Lieuwe41@users.noreply.github.com>
2019-06-27 14:30:03 +0200

    while True loop toe gevoeg + uitleg aan gepast + kan numbers in the right place aantonen.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 uitleg = "..."
 print(uitleg)
 
-#random getalen code maken
 from random import randint
 code = []
 for i in range(0, 4):
   code.append((randint(0,6)))
 print(code)
 
-#niput van user vragen
-UserInput = input("Type your guess: ")
-print(UserInput)
-if UserInput == code:
-  print ("You solved the code!")
-else:
-  print ("hoi")
+while True:
+  UserInput = input("Type your guess: ")
+  if UserInput == code:
+    print("You solved the code!")
+  else:
+    g = 0
+    for j in range(0, 4):
+      if str(code[j]) == (UserInput[j]):
+        g = g + 1
+    if g != 4:
+      print(str(g) + " numbers are in the right place")
