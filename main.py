624c416a4069efd7ca2ae01762f09c8cc5ebbcc5
octopoes <45490891+Lieuwe41@users.noreply.github.com>
2019-06-28 16:16:37 +0200

    Gaat ook beurt er af als je UserInput niet 4 nummers lang is of een letter bevat

--- main.py
+++ main.py
@@ -20,9 +20,13 @@ while True:
       print("You solved the code!")
     if len(UserInput) != 4:
       print("Error: Your Input must consist of 4 characters")
+      i = i - 1
+      print(str(i) + " chances left")
       continue
     if not UserInput.isdigit():
       print('Error: Your Input should be only numbers')
+      i = i - 1
+      print(str(i) + " chances left")
       continue
     else:
       g = 0
